824a55ac88d1db63ab8ee63e59648604c819d264
Andrew Arrow <andrew@anybeat.com>
2012-05-03 09:17:59 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magicbooks::Application.config.secret_token = '4d462261d24a23ac1a71387b3b7a9c6cd9c4af46055845a6ae30d20bdb450b0c48a98add9dabdd6e0afdddabb1c077c406418125bcf95a958ff3581670ed2533'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Magicbooks::Application.config.session_store :cookie_store, key: '_magicbooks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Magicbooks::Application.config.session_store :active_record_store
